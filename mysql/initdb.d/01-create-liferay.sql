8ba7fb9202e14695688bef17c41b4478a3a38c20
Caio Farias <caio.farias@liferay.com>
2026-02-04 18:11:43 -0300

    make 01-create-liferay-sql script credentials easier to override using sed

--- mysql/initdb.d/01-create-liferay.sql
+++ mysql/initdb.d/01-create-liferay.sql
@@ -1,6 +1,7 @@
 -- Initializes the Liferay database user on first container start.
 -- Adjust the password if you change JNDI_DB_PASSWORD in docker-compose.
 DROP USER IF EXISTS 'liferay'@'%';
-CREATE USER 'liferay'@'%' IDENTIFIED BY 'liferay' REQUIRE SSL;
+CREATE USER 'liferay'@'%' 
+    IDENTIFIED BY 'liferay' REQUIRE SSL;
 GRANT ALL PRIVILEGES ON lportal.* TO 'liferay'@'%';
 FLUSH PRIVILEGES;
